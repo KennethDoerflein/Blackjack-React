29ceefd7bf05c35486ca546a3afbd3cacc4a59c9
Kenneth Doerflein <ktd500@verizon.net>
2024-08-22 03:20:32 -0400

    Wrap game button function calls in anonymous functions

--- blackjack/src/components/GameControls.js
+++ blackjack/src/components/GameControls.js
@@ -16,21 +16,21 @@ export default function GameControls({ hit, newGame, endHand, doubleDownAllowed,
   return (
     <>
       <div id="gameActions" className="d-flex justify-content-center w-100 mt-2">
-        <button onClick={hit} hidden id="hitBtn" type="button" className="btn-sm btn btn-warning mx-2">
+        <button onClick={() => hit()} hidden id="hitBtn" type="button" className="btn-sm btn btn-warning mx-2">
           Hit
         </button>
-        <button onClick={splitHand} hidden id="splitBtn" type="button" className="btn-sm btn btn-primary mx-2">
+        <button onClick={() => splitHand()} hidden id="splitBtn" type="button" className="btn-sm btn btn-primary mx-2">
           Split
         </button>
-        <button onClick={doubleDown} hidden id="doubleDownBtn" type="button" className="btn-sm btn btn-light mx-2">
+        <button onClick={() => doubleDown()} hidden id="doubleDownBtn" type="button" className="btn-sm btn btn-light mx-2">
           Double
           <br />
           Down
         </button>
-        <button onClick={endHand} hidden id="standBtn" type="button" className="btn-sm btn btn-danger mx-2">
+        <button onClick={() => endHand()} hidden id="standBtn" type="button" className="btn-sm btn btn-danger mx-2">
           Stand
         </button>
-        <button onClick={newGame} hidden id="newGameBtn" type="button" className="btn-sm btn btn-success mx-auto my-1">
+        <button onClick={() => newGame()} hidden id="newGameBtn" type="button" className="btn-sm btn btn-success mx-auto my-1">
           New Game
         </button>
       </div>
