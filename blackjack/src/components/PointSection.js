9683a18aa274f666dc836ff050569a80feb5e2b9
Kenneth Doerflein <ktd500@verizon.net>
2024-08-25 16:14:57 -0400

    Adjust `pointSection' classes

--- blackjack/src/components/PointSection.js
+++ blackjack/src/components/PointSection.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container } from "react-bootstrap";
 
 export default function PointSection({ playerPoints, currentWager }) {
   return (
     <Container className="mt-2 d-flex justify-content-center">
-      <Row>
-        <Col className="text-center me-3 mb-2 px-4 border rounded d-flex align-items-center justify-content-center" id="pointsDisplay">
-          Points*: {playerPoints}
-        </Col>
-        <Col className="text-center ms-3 mb-2 px-2 border rounded d-flex align-items-center justify-content-center" id="wagerDisplay">
-          Current Hand's Wager: {currentWager}
-        </Col>
-      </Row>
+      <Container className="text-center me-3 mb-2 px-4 border rounded d-flex align-items-center justify-content-center" id="pointsDisplay">
+        Points*: {playerPoints}
+      </Container>
+      <Container className="text-center ms-3 mb-2 px-2 border rounded d-flex align-items-center justify-content-center" id="wagerDisplay">
+        Current Hand's Wager: {currentWager}
+      </Container>
     </Container>
   );
 }
