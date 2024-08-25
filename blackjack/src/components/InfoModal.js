95cb8877d5dccc305e33dad7806d6771d6ba387f
Kenneth Doerflein <ktd500@verizon.net>
2024-08-25 15:09:01 -0400

    Convert info modal to react-bootstrap

--- blackjack/src/components/InfoModal.js
+++ blackjack/src/components/InfoModal.js
@@ -1,113 +1,90 @@
-import React, { useEffect } from "react";
-import { Modal } from "bootstrap";
+import React, { useState } from "react";
+import { Modal, Button } from "react-bootstrap";
 
 export default function InfoModal({ newGame }) {
-  useEffect(() => {
-    // Show the modal when the component mounts
-    const modalElement = document.getElementById("infoModal");
-    const modal = new Modal(modalElement, {
-      keyboard: false,
-    });
-    modal.show();
+  const [show, setShow] = useState(true);
 
-    // Cleanup function to remove the backdrop
-    const handleModalHidden = () => {
-      const backdrop = document.querySelector(".modal-backdrop");
-      if (backdrop) {
-        backdrop.parentNode.removeChild(backdrop);
-      }
-    };
-
-    modalElement.addEventListener("hidden.bs.modal", handleModalHidden);
-
-    return () => {
-      modalElement.removeEventListener("hidden.bs.modal", handleModalHidden);
-    };
-  }, []);
-
+  const handleClose = () => setShow(false);
   return (
-    <>
-      <div className="modal fade" id="infoModal" tabIndex="-1" aria-labelledby="infoModalLabel" aria-hidden="true" data-bs-backdrop="static" data-bs-keyboard="false">
-        <div className="modal-dialog modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header bg-primary text-white">
-              <h5 className="modal-title mx-auto fw-bolder" id="infoModalLabel">
-                Game Rules and Button Explanations
-              </h5>
-            </div>
-            <div className="modal-body bg-light text-dark">
-              <p className="modal-title text-center fw-bolder">
-                <u>Disclaimer</u>
-              </p>
-              <p>By visiting this site or playing this game, you acknowledge that:</p>
-              <ul>
-                <li>
-                  The points earned in this game are purely fictional, have no monetary value, and are for entertainment purposes only. They cannot be exchanged for any goods
-                  or services.
-                </li>
-                <li>This game does not promote any form of gambling.</li>
-                <li>The game is intended for users who are 18 years of age or older.</li>
-                <li>Users should play responsibly and ensure that playing does not interfere with their daily responsibilities.</li>
-                <li>The game rules are subject to change, so users should check the rules regularly.</li>
-                <li>By bypassing this agreement, you accept and agree to abide by these terms.</li>
-              </ul>
+    <Modal show={show} onHide={handleClose} centered backdrop="static" keyboard={false}>
+      <Modal.Header className="bg-primary text-white">
+        <Modal.Title className="mx-auto fw-bolder">Game Rules and Button Explanations</Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="bg-light text-dark">
+        <p className="modal-title text-center fw-bolder">
+          <u>Disclaimer</u>
+        </p>
+        <p>By visiting this site or playing this game, you acknowledge that:</p>
+        <ul>
+          <li>
+            The points earned in this game are purely fictional, have no monetary value, and are for entertainment purposes only. They cannot be exchanged for any goods or
+            services.
+          </li>
+          <li>This game does not promote any form of gambling.</li>
+          <li>The game is intended for users who are 18 years of age or older.</li>
+          <li>Users should play responsibly and ensure that playing does not interfere with their daily responsibilities.</li>
+          <li>The game rules are subject to change, so users should check the rules regularly.</li>
+          <li>By bypassing this agreement, you accept and agree to abide by these terms.</li>
+        </ul>
 
-              <hr />
+        <hr />
 
-              <p className="modal-title text-center fw-bolder">
-                <u>Blackjack Rules</u>
-              </p>
-              <ol>
-                <li>The goal of blackjack is to beat the dealer's hand without going over 21.</li>
-                <li>Face cards are worth 10. Aces are worth 1 or 11, whichever makes a better hand.</li>
-                <li>Each player starts with two cards; one of the dealer's cards is hidden until the end.</li>
-                <li>To 'Hit' is to ask for another card. To 'Stand' is to hold your total and end your turn.</li>
-                <li>If you go over 21, you bust, and the dealer wins regardless of the dealer's hand.</li>
-                <li>If you are dealt an Ace and a 10 (or a face card), you have blackjack.</li>
-                <li>
-                  The dealer will hit until their cards total 17 or higher. If the `soft17Switch` is checked, the dealer will also hit on a soft 17 (a hand totaling 17 with an
-                  Ace counted as 11).
-                </li>
-              </ol>
+        <p className="modal-title text-center fw-bolder">
+          <u>Blackjack Rules</u>
+        </p>
+        <ol>
+          <li>The goal of blackjack is to beat the dealer's hand without going over 21.</li>
+          <li>Face cards are worth 10. Aces are worth 1 or 11, whichever makes a better hand.</li>
+          <li>Each player starts with two cards; one of the dealer's cards is hidden until the end.</li>
+          <li>To 'Hit' is to ask for another card. To 'Stand' is to hold your total and end your turn.</li>
+          <li>If you go over 21, you bust, and the dealer wins regardless of the dealer's hand.</li>
+          <li>If you are dealt an Ace and a 10 (or a face card), you have blackjack.</li>
+          <li>
+            The dealer will hit until their cards total 17 or higher. If the `soft17Switch` is checked, the dealer will also hit on a soft 17 (a hand totaling 17 with an Ace
+            counted as 11).
+          </li>
+        </ol>
 
-              <hr />
+        <hr />
 
-              <p className="modal-title text-center fw-bolder">
-                <u>Button Explanations</u>
-              </p>
-              <ul>
-                <li>
-                  <strong>Hit Button</strong>: Request another card from the dealer.
-                </li>
-                <li>
-                  <strong>Split Button</strong>: Split two cards of the same value into two separate hands, doubling your wager. Splitting is allowed if the game is in
-                  progress, you haven't already split three times, the two cards have the same point value, and you have enough points for the wager.
-                </li>
-                <li>
-                  <strong>Double Down Button</strong>: Double your initial wager and receive one more card. This is typically used when the odds are in your favor. Doubling
-                  down is allowed if the game is in progress, your hand has exactly two cards, the total of the hand is 21 or less, and you have enough points for the wager.
-                </li>
-                <li>
-                  <strong>Stand Button</strong>: Choose not to take any more cards and pass to the next hand or dealer. If the `standSwitch` is checked and your total is 21,
-                  the game will automatically move to the dealer's turn or next hand.
-                </li>
-                <li>
-                  <strong>Start/New Game Button</strong>: Starts a new game.
-                </li>
-                <li>
-                  <strong>Music Toggle</strong>: Enables or disables the game music (disabled by default).
-                </li>
-              </ul>
-            </div>
-
-            <div className="modal-footer bg-primary">
-              <button onClick={newGame} type="button" className="mx-auto btn btn-warning" data-bs-dismiss="modal">
-                I Acknowledge and Accept
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+        <p className="modal-title text-center fw-bolder">
+          <u>Button Explanations</u>
+        </p>
+        <ul>
+          <li>
+            <strong>Hit Button</strong>: Request another card from the dealer.
+          </li>
+          <li>
+            <strong>Split Button</strong>: Split two cards of the same value into two separate hands, doubling your wager. Splitting is allowed if the game is in progress, you
+            haven't already split three times, the two cards have the same point value, and you have enough points for the wager.
+          </li>
+          <li>
+            <strong>Double Down Button</strong>: Double your initial wager and receive one more card. This is typically used when the odds are in your favor. Doubling down is
+            allowed if the game is in progress, your hand has exactly two cards, the total of the hand is 21 or less, and you have enough points for the wager.
+          </li>
+          <li>
+            <strong>Stand Button</strong>: Choose not to take any more cards and pass to the next hand or dealer. If the `standSwitch` is checked and your total is 21, the
+            game will automatically move to the dealer's turn or next hand.
+          </li>
+          <li>
+            <strong>Start/New Game Button</strong>: Starts a new game.
+          </li>
+          <li>
+            <strong>Music Toggle</strong>: Enables or disables the game music (disabled by default).
+          </li>
+        </ul>
+      </Modal.Body>
+      <Modal.Footer className="bg-primary">
+        <Button
+          onClick={() => {
+            newGame();
+            handleClose();
+          }}
+          variant="warning"
+          className="mx-auto">
+          I Acknowledge and Accept
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
