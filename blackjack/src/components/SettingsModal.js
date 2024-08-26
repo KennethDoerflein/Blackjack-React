db5a4ef75c1eca6a417ce7c7f7e0365d5e5568ee
Kenneth Doerflein <ktd500@verizon.net>
2024-08-25 22:50:53 -0400

    Temporarily exclude settings options

--- blackjack/src/components/SettingsModal.js
+++ blackjack/src/components/SettingsModal.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Modal, Button, Form } from "react-bootstrap";
-import { checkSplitButton } from "../utils/utils.js";
+// import { checkSplitButton } from "../utils/utils.js";
 
 export default function SettingsModal({ playersHands, currentHand, splitCount, currentWager, playerPoints, playerTotals, endHand, show, handleClose }) {
   const toggleMusic = () => {
@@ -10,11 +10,11 @@ export default function SettingsModal({ playersHands, currentHand, splitCount, c
     }
   };
 
-  const checkStand = () => {
-    if (playerTotals[currentHand] === 21) {
-      endHand();
-    }
-  };
+  // const checkStand = () => {
+  //   if (playerTotals[currentHand] === 21) {
+  //     endHand();
+  //   }
+  // };
 
   return (
     <Modal show={show} onHide={handleClose} centered>
@@ -27,7 +27,7 @@ export default function SettingsModal({ playersHands, currentHand, splitCount, c
         </div>
         <Form>
           <Form.Check type="switch" id="musicSwitch" label="Music" onClick={toggleMusic} />
-          <Form.Check type="switch" id="standSwitch" label="Automatically Stand When You Get 21" onClick={checkStand} />
+          {/* <Form.Check type="switch" id="standSwitch" label="Automatically Stand When You Get 21" onClick={checkStand} />
           <Form.Check defaultChecked type="switch" id="soft17Switch" label="Dealer Hits on Soft 17" disabled={currentWager[0] > 0} />
           <Form.Check
             type="switch"
@@ -35,7 +35,7 @@ export default function SettingsModal({ playersHands, currentHand, splitCount, c
             label="Split Based on Rank"
             onClick={() => checkSplitButton(playersHands, currentHand, splitCount, currentWager, playerPoints)}
             disabled={currentWager[0] > 0}
-          />
+          /> */}
         </Form>
       </Modal.Body>
       <Modal.Footer>
