7d90299e4c529db3ba737ff103676848ecc0ada3
Kenneth Doerflein <ktd500@verizon.net>
2024-10-09 15:30:54 -0400

    Fix build

--- blackjack/src/components/PlayerSection.jsx
+++ blackjack/src/components/PlayerSection.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Container, Carousel } from "react-bootstrap";
 import { adjustCardMargins } from "../utils/uiUtils.js";
 
