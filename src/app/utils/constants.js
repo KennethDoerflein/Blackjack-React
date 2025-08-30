9de4c47555b01de8a41dc3ae4a2581a8b59e9083
Kenneth Doerflein <98554038+KennethDoerflein@users.noreply.github.com>
2025-08-29 23:44:16 -0400

    adjust animation timing

--- src/app/utils/constants.js
+++ src/app/utils/constants.js
@@ -1,7 +1,7 @@
 // Centralized animation timings
 const speedup = -25; // ms
-export const CARD_SLIDE_TIME = 400 + speedup; // ms
-export const CARD_FLIP_TIME = 400 + speedup; // ms
+export const CARD_SLIDE_TIME = 444 + speedup; // ms
+export const CARD_FLIP_TIME = 444 + speedup; // ms
 export const CARD_PULSE_TIME = 250 + speedup; // ms
 export const BLACKJACK_PAUSE_TIME = 1750;
 export const UI_TRANSITION_DELAY = 100;
