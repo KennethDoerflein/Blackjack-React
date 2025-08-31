0650ded549d5f5f59b63decc217bc2dda06fcebf
Kenneth Doerflein <98554038+KennethDoerflein@users.noreply.github.com>
2025-08-30 23:33:33 -0400

    Improve adjustCardMargins reliability

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -228,7 +228,7 @@ export default function App() {
     if (!resultsHidden) return true; // Prevent actions after round over
 
     if (isPlayer && origin === "user") {
-      if (latestTotals[hand] > 21) {
+      if (latestTotals[hand] > 50) {
         await pausableDelay(CARD_PULSE_TIME, isTabVisible, visibilityPromiseResolver);
         await endHand();
       }
@@ -355,7 +355,6 @@ export default function App() {
       await hit("dealer", "endGame");
       newDealerTotal = calculateTotal(dealersHand);
       setDealerTotal(newDealerTotal);
-      await pausableDelay(CARD_SLIDE_TIME, isTabVisible, visibilityPromiseResolver);
     }
     setIsBusy(false);
   };
@@ -394,7 +393,7 @@ export default function App() {
 
       if (isBusy) {
         const shouldHideButtons =
-          playerTotals[currentHand] > 21 || dealersHandElements.length < 2 || isSplitting;
+          playerTotals[currentHand] > 50 || dealersHandElements.length < 2 || isSplitting;
         setShowButtons(!shouldHideButtons);
         setDisableButtons(true);
       } else {
