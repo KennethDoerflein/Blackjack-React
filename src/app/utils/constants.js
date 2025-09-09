13f3374a261f4a0d91792936bc1838a07eb0404a
Kenneth Doerflein <98554038+KennethDoerflein@users.noreply.github.com>
2025-09-09 12:15:04 -0400

    Update constants.js

--- src/app/utils/constants.js
+++ src/app/utils/constants.js
@@ -1,7 +1,7 @@
 // Centralized animation timings
 const slowdown = -180; // ms
 export const CARD_SLIDE_TIME = 600 + slowdown; // ms
-export const CARD_FLIP_TIME = 500 + slowdown; // ms
+export const CARD_FLIP_TIME = 500 + (0.5 * slowdown); // ms
 export const CARD_PULSE_TIME = 500 + slowdown; // ms
 export const BLACKJACK_PAUSE_TIME = 1750;
 export const UI_TRANSITION_DELAY = 100;
