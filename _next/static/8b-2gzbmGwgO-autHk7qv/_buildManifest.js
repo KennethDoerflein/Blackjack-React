1e214ede942bc82a9e210f06a5b7e628c3c5bcc6
KennethDoerflein <98554038+KennethDoerflein@users.noreply.github.com>
2026-03-18 00:57:24 +0000

    Deploying to gh-pages from @ KennethDoerflein/Blackjack-React@15ae84e09eba6825db1d0d3d7df4ad12eb33fd47 🚀

---
+++ _next/static/8b-2gzbmGwgO-autHk7qv/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Blackjack-React//_next/:path+",
+        "destination": "/Blackjack-React/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
